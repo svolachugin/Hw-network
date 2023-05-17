29c1e1ca42de01957940bba9fb6c128711933b8e
svolachugin <svolachugin@mail.ru>
2023-05-17 15:39:19 +0300

    задание 2. Игра в Города

--- src/main/java/ru/netology/Client.java
+++ src/main/java/ru/netology/Client.java
@@ -5,18 +5,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
-    private static final int PORT = 8080;
+    private static final int PORT = 8989;
     private static final String HOST = "localhost";
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         try (Socket clientSocket = new Socket(HOST, PORT);
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-            out.println("Client"); //output 3a
-            System.out.println(in.readLine()); //output 3c
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+        ) {
+            String incoming = in.readLine();
+            if (incoming.equals("???")) {
+                System.out.println("Введите название любого города: ");
+            } else {
+                System.out.println("Последний введенный город: " + incoming);
+                System.out.println("Введите название города: ");
+            }
+            String outgoing = scanner.nextLine();
+            out.println(outgoing);
+            System.out.println(in.readLine());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -8,21 +8,37 @@ import java.net.Socket;
 
 public class Server {
 
-    private static final int PORT = 8080;
+    private static final int PORT = 8989;
 
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-            System.out.println("Server started"); //output 1
+            System.out.println("Сервер стартовал");
+            String town = null;
+            String townNew;
             while (true) {
                 try (Socket clientSocket = serverSocket.accept();
                      PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-                    System.out.println("New connection accepted"); //output 2
-                    out.println(String.format("Hi %s, your port is %d", in.readLine(), clientSocket.getPort())); //output 3b
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+                ) {
+                    if (town == null) {
+                        out.println("???");
+                        town = in.readLine();
+                        out.println("ОК");
+                    } else {
+                        out.println(town);
+                        townNew = in.readLine();
+                        if (town.charAt(town.length() - 1) == townNew.charAt(0)) {
+                            town = townNew;
+                            out.println("ОК");
+                        } else {
+                            out.println("НЕ ОК");
+                        }
+                    }
                 }
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
         }
     }
 }
